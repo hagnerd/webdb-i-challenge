08ddb54a4dc24ecd436588d683e8a945dd3ee400
Matt Hagner <web@matthagner.com>
2019-08-21 19:17:58 -0500

    Implement GET handler for api/accounts

--- routes/accounts.js
+++ routes/accounts.js
@@ -4,7 +4,20 @@ const db = require("../data/dbConfig.js");
 const router = express.Router();
 
 // get all accounts
-router.get("/", async (req, res) => {});
+router.get("/", async (_req, res) => {
+  try {
+    const accounts = await db.select("*").from("accounts");
+
+    res.json({
+      accounts
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: "internal server error",
+      message: error.message
+    });
+  }
+});
 
 // get account by id
 router.get("/:id", (req, res) => {});
